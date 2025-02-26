e3ebc7da45a427a8cf541bd54517ce7b70ad94fd
Yu <yuna1313677@gmail.com>
2025-02-26 20:36:26 +0800

    [新增] 基本架構

--- yu-p/src/app/app.module.ts
+++ yu-p/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { Layout1Component } from './layout/layout-1/layout-1.component';
+import { HeaderComponent } from './com/header/header.component';
+import { FooterComponent } from './com/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Layout1Component,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
