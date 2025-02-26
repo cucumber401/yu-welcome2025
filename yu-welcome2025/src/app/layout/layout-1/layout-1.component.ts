06615ac0de22cb61b12500ef0f9da0ecd027ec5d
Yu <yuna1313677@gmail.com>
2025-02-26 21:07:39 +0800

    [新增] yu-welcome2025專案

---
+++ yu-welcome2025/src/app/layout/layout-1/layout-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-1',
+  templateUrl: './layout-1.component.html',
+  styleUrls: ['./layout-1.component.scss']
+})
+export class Layout1Component {
+
+}
