59ecc8205cfa5d61dd14889facf65ebd4bd7eea0
Yu <yuna1313677@gmail.com>
2025-03-08 01:00:32 +0800

    [新增] navbar(大)

--- yu-welcome2025/src/app/app-routing.module.ts
+++ yu-welcome2025/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './page/home/home.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { ExperienceComponent } from './page/experience/experience.component';
+import { ProjectsComponent } from './page/projects/projects.component';
+import { SkillsComponent } from './page/skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
 ];
 
 @NgModule({
--- yu-welcome2025/src/app/app.module.ts
+++ yu-welcome2025/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './page/home/home.component';
 import { Layout1Component } from './layout/layout-1/layout-1.component';
 import { HeaderComponent } from './com/header/header.component';
 import { FooterComponent } from './com/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { ExperienceComponent } from './page/experience/experience.component';
+import { SkillsComponent } from './page/skills/skills.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { ProjectsComponent } from './page/projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { FooterComponent } from './com/footer/footer.component';
     HomeComponent,
     Layout1Component,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ContactComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
