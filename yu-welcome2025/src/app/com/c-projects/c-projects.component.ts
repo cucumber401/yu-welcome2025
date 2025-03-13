138b30ed6d6236f61366608b7c017bcad78866ec
Yu <yuna1313677@gmail.com>
2025-03-13 20:51:03 +0800

    [新增] 作品集影片

--- yu-welcome2025/src/app/com/c-projects/c-projects.component.ts
+++ yu-welcome2025/src/app/com/c-projects/c-projects.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./c-projects.component.scss']
 })
 export class CProjectsComponent {
-
+  demoList: { title: string, description: string, src: string, sizeRatio: string }[] = [
+    {title: 'BA 前後對比圖', description: '學會有思兒氣本母時兩士去子西亞！際在步待時里而下你頭北，音的加？來然命走從防了信：我列。', src:'../../../assets/videos/demo_BA圖.mp4', sizeRatio: '1920 / 1336'},
+    {title: '生成作品展示', description: '學會有思兒氣本母時兩士去子西亞！際在步待時里而下你頭北，音的加？來然命走從防了信：我列。', src:'../../../assets/videos/demo_生成作品展示.mp4', sizeRatio: '1920 / 1740'},
+    {title: '資料上傳', description: '學會有思兒氣本母時兩士去子西亞！際在步待時里而下你頭北，音的加？來然命走從防了信：我列。', src:'../../../assets/videos/demo_上傳資料.mp4', sizeRatio: '1920 / 1680'},
+  ];
 }
