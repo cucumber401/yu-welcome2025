aa502a8914d595cc5aacae795377db7173b15d44
Yu <yuna1313677@gmail.com>
2025-03-23 21:43:11 +0800

    [新增] goToTop按鈕

--- yu-welcome2025/src/app/page/single-page-resume/single-page-resume.component.ts
+++ yu-welcome2025/src/app/page/single-page-resume/single-page-resume.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-page-resume.component.scss']
 })
 export class SinglePageResumeComponent {
-
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
